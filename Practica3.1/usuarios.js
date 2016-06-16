ef8e31204be91fb9ff5e4038d7a421c1d4456261
Daniel Marcos <danielmarcos.to@gmail.com>
2016-06-16 12:27:17 +0200

    Added practice 3.1

---
+++ Practica3.1/usuarios.js
@@ -0,0 +1,43 @@
+{
+  "usuarios": [
+    {
+      "nombre": "Pepo",
+      "apellido": "Garcia",
+      "email": "papo@gmail.com",
+      "telefono": "4126448426",
+      "trabajo": [
+        {
+          "direccion": "oficina",
+          "telefono_oficina": "2514821233",
+          "cargo": "desarrollador"
+        }
+      ]
+    },
+    {
+      "nombre": "Ezio",
+      "apellido": "Auditore",
+      "email": "AC@gmail.com",
+      "telefono": "4126448426",
+      "trabajo": [
+        {
+          "direccion": "Italia",
+          "telefono_oficina": "2514821233",
+          "cargo": "desarrollador"
+        }
+      ]
+    },
+    {
+      "nombre": "Leandro",
+      "apellido": "Martinez",
+      "email": "whoa@gmail.com",
+      "telefono": "4126448426",
+      "trabajo": [
+        {
+          "direccion": "Italia",
+          "telefono_oficina": "2514821233",
+          "cargo": "desarrollador"
+        }
+      ]
+    }
+  ]
+}
